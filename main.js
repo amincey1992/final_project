f53a13d12e614ed2c0b6ce77b0fc3b0fba013fa9
alex <amincey1992@gmail.com>
2016-12-20 15:22:32 -0500

    work

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 /**
  * Created by Mincey on 12/1/16.
  */
-
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 
 var question = [];
